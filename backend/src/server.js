880711e4d6c469580f6ff258b95927d73e3a7c21
Ahmed-Ebrahim-23 <ahmed2322004ahmed@gmail.com>
2025-12-28 22:33:05 +0200

    refactor: seperated prisma config for easier import

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,30 @@
 require('dotenv').config();
 const express = require('express');
-const { PrismaClient } = require('@prisma/client');
 
 const app = express();
-const prisma = new PrismaClient();
 const port = process.env.PORT;
+
+const authRouter = require("./routers/auth.router");
+const userRouter = require("./routers/user.router");
+const taskRouter = require("./routers/task.router");
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`${req.method} http://localhost:${port}${req.path}`);
+  next();
+});  
+
+app.use('/auth',authRouter);
+app.use('/users',userRouter);
+app.use('/tasks',taskRouter);
+
+app.use((req, res) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on the server`
+  });
+});
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
